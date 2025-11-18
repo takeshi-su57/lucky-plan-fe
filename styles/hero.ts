284792a7ba3a762443eccdea83b255911b06840a
takeshi <takeshisuz57@gmail.com>
2025-11-18 14:57:59 -0500

    fix: style issue

--- styles/hero.ts
+++ styles/hero.ts
@@ -2,4 +2,6 @@
 import { heroui } from "@heroui/react";
 // or import from theme package if you are using individual packages.
 // import { heroui } from "@heroui/theme";
-export default heroui();
+export default heroui({
+  addCommonColors: true,
+});
