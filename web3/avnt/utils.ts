6de724712f540bfa8362a36de8b9edc2c7c0732f
takeshi <takeshisuz57@gmail.com>
2026-07-06 17:51:55 -0400

    chore update utils for the avnt

--- web3/avnt/utils.ts
+++ web3/avnt/utils.ts
@@ -1,8 +1,12 @@
-export function getAvntPositionKey(address: string, index: number) {
-  return JSON.stringify({ address: address.toLowerCase(), index });
+export function getAvntPositionKey(
+  address: string,
+  pairIndex: number,
+  index: number,
+) {
+  return JSON.stringify({ address: address.toLowerCase(), pairIndex, index });
 }
 
 export function parseAvntPositionKey(positionKey: string) {
-  const { address, index } = JSON.parse(positionKey);
-  return { address, index: Number(index) };
+  const { address, pairIndex, index } = JSON.parse(positionKey);
+  return { address, pairIndex: Number(pairIndex), index: Number(index) };
 }
